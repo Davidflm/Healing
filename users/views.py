d67a9e0d0560431d1d0ad0002bd593692b2f5691
David Flávio <davidflaviolm1982@gmail.com>
2024-04-20 18:22:40 -0300

    Implemented login filters and mapped datas

--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def registry(request):
-  return render(request, 'registry.html')
+  if request.method == 'GET':
+    return render(request, 'registry.html')
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    confirmar_senha = request.POST.get('confirmar_senha')
+    
+    if senha != confirmar_senha:
+      print('Erro 2')
+      return redirect('/users/register')
+    
+    if len.senha < 6:
+      print('Erro 3')
+      return redirect('/users/register')
+    
+    return HttpResponse(f"{username} ---{email}----{senha}----{confirmar_senha}")
