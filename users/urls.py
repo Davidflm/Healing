dd44d97d7bd3a7c0b31881a5f2ca8afcf9e31af9
David Flávio <davidflaviolm1982@gmail.com>
2024-04-20 22:09:39 -0300

    Create View Login and implemented funcion Logar

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('register/', views.registry, name='registry')
+  path('register/', views.registry, name='registry'),
+  path("login/", views.login_view, name="login")
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 def registry(request):
@@ -34,4 +35,19 @@ def registry(request):
       password=senha,
     )
     
+    return redirect('/users/login')
+
+def login_view(request):
+  if request.method == 'GET':
+    return render(request, 'login.html')
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    senha = request.POST.get('senha')
+  
+    user = auth.authenticate(request, username=username, password=senha)
+    
+    if user:
+      auth.login(request, user)
+      return redirect('pacientes/home')
+    messages.add_message(request, constants.ERROR, 'Usuário ou Senha inválida')
     return redirect('/users/login')
